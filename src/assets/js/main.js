3a83294034ebdb6838b2f49faa6572e0eef5b9ef
AlexSokol <astrasokol@yandex.ru>
2025-06-19 07:23:35 +0300

    подключил стили через js

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+// alert('JS файл подключен!');
+
+/* Импорт компонентов */
+import '../css/components/buttons.css';
+import '../css/components/forms.css';
+import '../css/components/cards.css';
+import '../css/components/modals.css';
+
+/* Импорт стилей компонентов */
+import '../css/layout/header.css';
+import '../css/layout/footer.css';
+import '../css/layout/mobile-menu.css';
+import '../css/layout/navigation.css';
+import '../css/layout/social-buttons.css';
+
+/* Импорт стилей страниц */
+import '../css/pages/about.css';
+import '../css/pages/home.css';
+import '../css/pages/equipment.css';
+import '../css/pages/contacts.css';
+import '../css/pages/analog.css';
+import '../css/pages/partners.css';
+
+// Главный CSS (импортируется последним, чтобы переопределять остальные при необходимости)
+import '../css/main.css';
